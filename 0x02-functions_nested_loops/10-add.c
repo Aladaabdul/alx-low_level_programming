5fa11bf921d75e15dcf9a399753b047f1f099edf
Yakub <aladarahman@gmail.com>
2023-07-19 10:16:33 -0700

    add two integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * add - adds two integers
+ * @a: formal parameter
+ * @n: formal parameter
+ * Return: 0
+ */
+int add(int a, int n)
+{
+	int cal = int a + int n;
+
+	return (call);
+}
