d3bf1cfabdeb52b6dc71a3c2411bdc5782b66f21
Yakub <aladarahman@gmail.com>
2023-07-19 16:05:09 -0700

    fibo

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 /**
  * main - print fibo numbers
- *@n: Formal parameter
  * Return: always 0
  */
 int main(void)
 {
 	int fib1 = 1, fib2 = 2, fib_next, i, n;
+
+	n = 50;
 
 	printf("%d, %d, ", fib1, fib2);
 	for (i = 2; i < n; i++)
