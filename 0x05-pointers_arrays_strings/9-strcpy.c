8c9d8b80c20582a9f1c458a602ae4edc62ab3e9b
Yakub <aladarahman@gmail.com>
2023-07-26 22:37:23 -0700

    string copy

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -7,10 +7,12 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-	while (str[i] != '\0')
+	int i = 0;
+
+	while (src[i] != '\0')
 	{
 		dest[i] = src[i];
-		i++
+		i++;
 	}
 	dest[i] = ('\0');
 	return (dest);
