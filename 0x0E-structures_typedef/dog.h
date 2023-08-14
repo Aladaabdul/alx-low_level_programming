aef84c40235d4929535f9da4123f946caa95606f
aladarahman <aladarahman@gmail.com>
2023-08-14 18:33:07 +0100

    Define a new type struct dog with the following elements

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
+/**
+ * struct dog - Dog
+ * @name: member
+ * @age: member
+ * @owner: member
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
