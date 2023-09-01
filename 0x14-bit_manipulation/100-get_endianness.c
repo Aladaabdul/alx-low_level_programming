d896b9460c636b750acf7a4488621a49e5279fd3
aladarahman <aladarahman@gmail.com>
2023-09-01 13:28:34 +0100

    function that checks the endianness

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * get_endianness - checks the endianness
+ * Return: 0
+ */
+int get_endianness(void)
+{
+	unsigned int num = 1;
+	char *byte = (char *) & num;
+
+	if (*byte == 1)
+	{
+		return (1);
+	}
+	else 
+	{
+		return (0);
+	}
+}
